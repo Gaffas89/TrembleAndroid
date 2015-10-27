a64b21407711fca73813f7e163be6b3e4e20313e
Gaffas <gaffas.89@gmail.com>
2015-10-27 10:46:39 +0300

    initial models created

--- app/src/main/java/com/techzonecs/tremble/model/Trainee.java
+++ app/src/main/java/com/techzonecs/tremble/model/Trainee.java
@@ -11,4 +11,52 @@ public class Trainee {
     private String email;
     private String grade;
     private String subject;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
