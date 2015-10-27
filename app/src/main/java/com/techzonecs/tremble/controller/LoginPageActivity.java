f4805641b023cb84c02e483a5ec0ec3ced4842a8
aisha-al-ansari@hotmail.com <aisha-al-ansari@hotmail.com>
2015-10-27 15:27:51 +0400

    layout

---
+++ app/src/main/java/com/techzonecs/tremble/controller/LoginPageActivity.java
@@ -0,0 +1,15 @@
+package com.techzonecs.tremble.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.techzonecs.tremble.R;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
